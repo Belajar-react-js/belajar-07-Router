b25370669394384a4b277b42db3e96b9df2526a9
kharisma tara mukti <kharismataram@gmail.com>
2023-05-16 15:56:18 +0700

    cek stunting

---
+++ src/pages/CekStunting.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "react-bootstrap";
+import NavbarSipenting from "../components/Navbar";
+import Hero from "../components/Hero";
+
+function CekStunting() {
+  return (
+    <>
+      <NavbarSipenting />
+      <Hero />
+      <h1 className="text-center"> Cek Stunting </h1>
+    </>
+  );
+}
+export default CekStunting;
