03104e74186dff5af5387a0f12d199722ebb978a
kharisma tara mukti <kharismataram@gmail.com>
2023-05-16 20:38:01 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Artikel from "./pages/Artikel";
 
 import { Routes, Route } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Beranda />} />
         <Route path="/cekstunting" element={<CekStunting />} />
         <Route path="/artikel" element={<Artikel />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
